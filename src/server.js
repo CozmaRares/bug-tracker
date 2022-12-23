89b59500852598684eb1a525bd5215dba18c0b73
Cozma Rares <cozmarares11@gmail.com>
2022-12-23 16:10:47 +0200

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const port = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.resolve(__dirname, "..", "public")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404! Page not found</h1>");
+});
+
+app.listen(port, error => {
+  console.log(error ? error : "Server listening on http://localhost:" + port);
+});
